ef140b966fdec819831938c3e933248434256707
vdanushapradeep <vdanushapradeep@gmail.com>
2026-06-12 15:25:16 +0530

    Replace @Data with @Getter in ErrorResponse

--- src/main/java/com/example/rewards/exception/ErrorResponse.java
+++ src/main/java/com/example/rewards/exception/ErrorResponse.java
@@ -3,13 +3,13 @@ package com.example.rewards.exception;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 /** Standardized error response payload returned by the API. */
-@Data
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
